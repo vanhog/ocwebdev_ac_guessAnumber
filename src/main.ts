97f13e57e400467ee6204a667f376ec41e4b6322
vanhog <dieter@hoogestraat.dk>
2026-01-27 16:32:48 +0100

    Basic functionality

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,61 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+let inputField = document.getElementById('number_input') as HTMLInputElement;
+let guessButton = document.getElementById('guessButton');
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let gameRuns: number = 0;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function getValidGuess(): number {
+  let guess: number = NaN;
+  do {
+    guess = Number(inputField.value);
+    if (isNaN(guess) || guess > 100 || guess < 1) {
+      window.alert('To win, please, input a number between 1 and 100.');
+      inputField.value = ' ';
+    }
+  } while (isNaN(guess));
+  return guess;
+}
+
+function getNumGuesses(): number {
+  const checkedRadio = document.querySelector<HTMLInputElement>(
+    'input[name="guesses"]:checked',
+  );
+  let attempts: number = Number(checkedRadio?.value) || NaN;
+
+  if (isNaN(attempts)) {
+    let customAttempts: number = 0;
+    do {
+      customAttempts =
+        Number(
+          prompt('How many times do you want to guess the right number?'),
+        ) || NaN;
+    } while (isNaN(customAttempts));
+    attempts = customAttempts;
+  }
+  return attempts;
+}
+
+guessButton?.addEventListener('click', function () {
+  if (!(gameRuns > 0)) {
+    gameRuns = getNumGuesses();
+  }
+
+  // disable radio buttons to avoid distraction
+  const radios = document.querySelectorAll<HTMLInputElement>(
+    'input[name="guesses"]',
+  );
+  radios.forEach((radio) => (radio.disabled = true));
+
+  for (let i = 1; i <= gameRuns; i++) {
+    let guess = getValidGuess();
+    if (guess === 5) {
+      window.alert('You won!');
+      inputField.value = '';
+      break;
+    } else {
+      guess = getValidGuess();
+    }
+    console.log(i, gameRuns, guess, inputField.value);
+  }
+  radios.forEach((radio) => (radio.disabled = false));
+  gameRuns = 0;
+});
